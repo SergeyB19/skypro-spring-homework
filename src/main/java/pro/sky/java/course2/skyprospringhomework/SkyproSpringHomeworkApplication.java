de91589a16b53139b9474f0a7b14c28bb73226c7
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-07-18 17:04:22 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/skyprospringhomework/SkyproSpringHomeworkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.skyprospringhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringHomeworkApplication.class, args);
+	}
+
+}
