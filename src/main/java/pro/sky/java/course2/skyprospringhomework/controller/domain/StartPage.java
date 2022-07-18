b567f1bdb4d28ff8b4ad94fdb317e2fb30004f58
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-07-18 18:45:54 +0300

    springHomework

---
+++ src/main/java/pro/sky/java/course2/skyprospringhomework/controller/domain/StartPage.java
@@ -0,0 +1,37 @@
+package pro.sky.java.course2.skyprospringhomework.controller.domain;
+
+public class StartPage {
+    private String login;
+    private String password;
+    private String confirmPassword;
+
+    public StartPage(String login, String password, String confirmPassword) {
+        this.login = login;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
