e1892445eef495d8a4fd98981ddaf48cd1242e28
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-07-19 09:16:11 +0300

    springHomework

--- src/main/java/pro/sky/java/course2/skyprospringhomework/controller/Controller.java
+++ src/main/java/pro/sky/java/course2/skyprospringhomework/controller/Controller.java
@@ -3,7 +3,6 @@ package pro.sky.java.course2.skyprospringhomework.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import pro.sky.java.course2.skyprospringhomework.controller.domain.StartPage;
 import pro.sky.java.course2.skyprospringhomework.controller.domain.service.StartPageService;
 import pro.sky.java.course2.skyprospringhomework.controller.domain.service.exceptions.WrongLoginException;
 import pro.sky.java.course2.skyprospringhomework.controller.domain.service.exceptions.WrongPasswordException;
@@ -17,21 +16,48 @@ public class Controller {
     }
 
     @GetMapping("/login")
-    public static void login(@RequestParam(name = "login") String a) throws WrongLoginException {
-        if ( a <= 20;) {
+    public static String login() throws WrongLoginException {
+        if (a <= 20) {
             throw new WrongLoginException("Неверный логин");
+            try {
+                login();
+
+            } catch (WrongLoginException) {
+                return "Неверный логин";
+            } finally {
+                System.out.println("Проверка завершена");
             }
         }
-    @GetMapping("/password ")
-    public static void login(@RequestParam(name = "password ") String a) throws WrongPasswordException {
-        if ( a <= 20;) {
-            throw new WrongPasswordException("Неверный пароль");
-        }
-        @GetMapping("/confirmPassword ")
-        public static void login(@RequestParam(name = "confirmPassword") String a) throws WrongPasswordException {
-            if ( a <= 20;) {
+
+
+        @GetMapping("/password ")
+        public static void login (@RequestParam(name = "password ") String a) throws WrongPasswordException {
+            if ( a <= 20;){
                 throw new WrongPasswordException("Неверный пароль");
             }
+            try {
+                login();
+
+            } catch (WrongLoginException) {
+                return "Неверный пароль";
+            } finally {
+                System.out.println("Проверка завершена");
+            }
+        }
+            @GetMapping("/confirmPassword ")
+            public static void login (@RequestParam(name = "confirmPassword") String a) throws WrongPasswordException {
+                if ( a <= 20;){
+                    throw new WrongPasswordException("Неверный пароль");
+                } try {
+                login();
+
+            } catch (WrongLoginException) {
+                return "Неверный пароль";
+            } finally {
+                System.out.println("Проверка завершена");
+            }
+            }
+        }
     }
-    }
+
 
